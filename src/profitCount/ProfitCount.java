18e944972b635f601611a1d991a9ddfd170e2698
damionjestem <ndnd@protonmail.ch>
2018-11-25 23:46:18 +0100

    Add exception handling

--- src/profitCount/ProfitCount.java
+++ src/profitCount/ProfitCount.java
@@ -15,22 +15,38 @@ public class ProfitCount {
     public static void main(String[] args) {
 
         System.out.println("Podaj kwotę bazową");
-        Scanner sc = new Scanner(System.in);
-        int baseAmount = Integer.parseInt(sc.nextLine());
+        int baseAmount = scannerIntInput();
 
         System.out.println("Podaj ile procent w skali roku");
-        sc = new Scanner(System.in);
-        int percent = Integer.parseInt(sc.nextLine());
+        int percent = scannerIntInput();
 
         System.out.println("Podaj ile miesięcy");
-        sc = new Scanner(System.in);
-        int months = Integer.parseInt(sc.nextLine());
+        int months = scannerIntInput();
 
         for (int i = 0; i <= months; i++) {
-            baseAmount += (baseAmount * percent/100) / 12;
+            baseAmount += (baseAmount * percent / 100) / 12;
         }
 
         System.out.println(baseAmount);
+    }
+
+    private static int scannerIntInput() {
+        Scanner sc = new Scanner(System.in);
+        int result = 0;
+        boolean isOk = false;
+        while (!isOk) {
+            try {
+                result = Integer.parseInt(sc.nextLine());
+                if (result > 0) {
+                    isOk = true;
+                } else {
+                    System.out.println("Liczba musi być większa od 0, wprowadź jeszcze raz");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Niepoprawny format liczby, wprowadź jeszcze raz");
+            }
+        }
+        return result;
     }
 
 }
