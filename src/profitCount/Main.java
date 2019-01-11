ca7940147990ba2b2a82a931f54132fa3c0c112a
damionjestem <ndnd@protonmail.ch>
2019-01-11 22:02:35 +0100

    Set example MyBankAccount instance to see if works

--- src/profitCount/Main.java
+++ src/profitCount/Main.java
@@ -5,12 +5,18 @@
  */
 package profitCount;
 
+import java.math.BigDecimal;
+
 /**
  *
  * @author Damion
  */
 public class Main {
     public static void main(String[] args) {
-        BankAccount bA = new BankAccount();
+        MyBankAccount mBA = MyBankAccount.getInstance();
+        
+        //test if it works
+        mBA.setBalance(BigDecimal.valueOf(10000));
+        System.out.println(new StringBuilder("Balance:").append(mBA.getBalance()));
     }
 }
